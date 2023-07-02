51ac0c8cda130847d8c8b20ed71873c6fe59500f
tazoakhalaia <darknemiroff@gmail.com>
2023-07-02 13:56:36 +0400

    Add request for events

---
+++ app/Http/Requests/EventsRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EventsRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'event_name' => 'required|min:3|max:30',
+            'event_description' => 'required|min:3|max:30',
+            'event_date' => 'required|min:3|max:30',
+            'event_location' => 'required|min:3|max:30'
+        ];
+    }
+}

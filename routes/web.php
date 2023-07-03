65eae9697c4b9db7de657ccb0a7d8663f0942de9
tazoakhalaia <darknemiroff@gmail.com>
2023-07-03 14:02:49 +0400

    feat/add google translate package

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Stichoza\GoogleTranslate\GoogleTranslate;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/translate', function() {
+    $tr = new GoogleTranslate('en');
+    $tr->setSource('en'); 
+    return $tr->setTarget('ka')->translate('hello world');
 });
